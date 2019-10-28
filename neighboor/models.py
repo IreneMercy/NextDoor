b725d00ed4df91a2359a9501d5b7c7c6863caa5f
IreneMercy <irenemercy700@gmail.com>
2019-10-28 14:19:10 +0300

    add business class

--- neighboor/models.py
+++ neighboor/models.py
@@ -9,9 +9,37 @@ class Neighborhood(models.Model):
     name = models.CharField(max_length=50)
     location = models.CharField(max_length=150)
     occupants = models.IntegerField()
+    def __str__(self):
+        return f'{self.user} Neighborhood'
 
+    class Meta:
+        db_table ='Neighborhood'
 
 class Profile(models.Model):
     name = models.CharField(max_length=50)
     neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE)
     image = models.ImageField()
+
+    def __str__(self):
+        return f'{self.user} Profile'
+
+    class Meta:
+        db_table ='Profile'
+
+class Business(models.Model):
+    name = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return f'{self.user} Business'
+
+    class Meta:
+        db_table ='Business'
+
+
+    @classmethod
+    def search_by_id(cls,search_term):
+        business = cls.objects.filter(id__icontains=search_term)
+        return business
