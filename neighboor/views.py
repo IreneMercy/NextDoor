0a05cc17ebecec5d019f34312a36ef28e59a6a03
IreneMercy <irenemercy700@gmail.com>
2019-10-28 10:01:45 +0300

    add home route

--- neighboor/views.py
+++ neighboor/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import *
+from .models import *
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+def registration(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            form.save()
+            return redirect('/login')
+    else:
+        form = RegisterForm()
+    context = {
+        'form':form,
+    }
+    return render(request, 'users/register.html', context)
