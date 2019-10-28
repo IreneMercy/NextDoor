edfc5f063f63e19535200d13949bad5907a62402
IreneMercy <irenemercy700@gmail.com>
2019-10-28 14:20:49 +0300

    add search url

--- neighboor/urls.py
+++ neighboor/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('register/', views.registration, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('search/', views.search_business, name="search"),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
