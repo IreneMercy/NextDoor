af27d188bffbdeb34590b9f720cb0356af27e24e
IreneMercy <irenemercy700@gmail.com>
2019-10-28 12:27:35 +0300

    add  neighborhood class

--- neighboor/models.py
+++ neighboor/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Neighborhood(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=150)
+    occupants = models.IntegerField()
