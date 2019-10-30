f37d4f08d3eb854cc130f5c5bf2b8c390671003a
IreneMercy <irenemercy700@gmail.com>
2019-10-30 18:48:08 +0300

    add updateprofile, poststory, addbusiness and add neighborhood forms

--- neighboor/forms.py
+++ neighboor/forms.py
@@ -9,3 +9,39 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['name', 'neighborhood']
+
+
+
+class StoryForm(forms.ModelForm):
+
+    class Meta:
+        model= Post
+        fields= ['title', 'story', 'neighborhood']
+
+
+class NeighborhoodForm(forms.ModelForm):
+
+    class Meta:
+        model = Neighborhood
+        fields = ['name', 'location',  'occupants', 'health_department_contact', 'police_authority_contact', 'image']
+
+
+class BusinessForm(forms.ModelForm):
+
+    class Meta:
+        model = Business
+        fields = ['name', 'email',  'neighborhood']
