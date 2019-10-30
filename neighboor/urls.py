5ec84fad4a2ffc5d11fee4911ec20da71e0f3c5c
IreneMercy <irenemercy700@gmail.com>
2019-10-30 18:42:12 +0300

    add url to route all templates

--- neighboor/urls.py
+++ neighboor/urls.py
@@ -11,6 +11,15 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('search/', views.search_business, name="search"),
+    path('profile/', views.profile, name='profile'),
+    path('updateprofile/', views.updateprofile, name='updateprofile'),
+    path('post/', views.poststory, name='poststory'),
+    path('hood/', views.createhood, name='hood'),
+    path('business/', views.createbusiness, name='business'),
+
+
+
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
