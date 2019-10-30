9ba5d6b8832de48a754b8c3a37b1b584539a6fd8
IreneMercy <irenemercy700@gmail.com>
2019-10-30 18:47:16 +0300

    add save and delete functions to classes in model file

--- neighboor/models.py
+++ neighboor/models.py
@@ -9,27 +9,53 @@ class Neighborhood(models.Model):
     name = models.CharField(max_length=50)
     location = models.CharField(max_length=150)
     occupants = models.IntegerField()
+    image = models.ImageField(default='hood.jpg', upload_to='hood/')
+    datecreated = models.DateTimeField(default=timezone.now)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='hood')
+    health_department_contact = models.IntegerField()
+    police_authority_contact = models.IntegerField()
+
     def __str__(self):
-        return f'{self.user} Neighborhood'
+        return f'{self.name} Neighborhood'
 
     class Meta:
         db_table ='Neighborhood'
 
+    def save_neighborhood(self):
+        self.save()
+
+    def delete_neighborhood(self):
+        self.delete()
+
 class Profile(models.Model):
-    name = models.CharField(max_length=50)
-    neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE)
-    image = models.ImageField()
+    name = models.CharField(max_length=50, null=True)
+    email = models.CharField(max_length=255)
+    user= models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    neighborhood = models.ForeignKey(Neighborhood, null=True, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.png', upload_to='profile/')
 
     def __str__(self):
-        return f'{self.user} Profile'
+        return f'{self.user.username} Profile'
 
     class Meta:
         db_table ='Profile'
 
+    @receiver(post_save, sender=User)
+    def update_create_profile(sender,instance,created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save,sender = User)
+    def save_profile(sender,instance,**kwargs):
+        instance.profile.save()
+
+
 class Business(models.Model):
     name = models.CharField(max_length=50)
+    email = models.CharField(max_length=255)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE)
+
 
 
     def __str__(self):
@@ -38,8 +64,33 @@ class Business(models.Model):
     class Meta:
         db_table ='Business'
 
+    def save_business(self):
+        self.save()
+
+    def delete_business(self):
+        self.delete()
+
 
     @classmethod
-    def search_by_id(cls,search_term):
-        business = cls.objects.filter(id__icontains=search_term)
+    def search_by_name(cls,search_term):
+        business = cls.objects.filter(name__icontains=search_term)
         return business
+
+
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    story = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE)
+
+
+    def save_post(self):
+        self.save()
+
+    def delete_post(self):
+        self.delete()
+
+    def __str__(self):
+        return f'{self.title} Post'
